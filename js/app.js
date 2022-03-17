07dd13a86d516a465165dfdd7635258ee802dbee
teresabozer <teresabozer@gmail.com>
2022-03-17 15:39:58 -0500

    update js for 1st lab

--- js/app.js
+++ js/app.js
@@ -39,67 +39,7 @@ if(title == 'Señor' || title == 'Sr.'){
   } 
 
 
-  'use strict';
-  console.log('app.js is connected.');
   
-  
-  // function functionName(){
-  //   console.log('we are in the function!!!!!!!!');
-  // }
-  // functionName();
-  
-  
-  function userName(){
-    let userName = prompt('What is your name?');
-    // console.log('userName is this =', userName);
-    return document.write(userName);
-  }
-  
-  
-  function timeOfDay() {
-    let userTime = prompt('What time is it?');
-    let message;
-  
-    if(userTime <= 11){
-      message = 'Good Morning';
-    } else if(userTime <= 18){
-      message = 'Good Afternoon';
-    } else if (userTime <= 24){
-      message = 'Good Evening';
-    } else {
-      message = 'Your input does not work for a time.';
-    } 
-    return message;
-  }
-
-
-
-  //alert is another method.
-
-
-
-//ask what time is it? 
-//variable assignment and declaration
-
-// console.log(userTime);
-
-//have some logic to respond to the user.
-// if(this is true){
-//  then we should do this.
-// } else if(this is true){
-//   we will do this.
-// } else {
-//   lets just do this.
-// }
-
-// variable declaration creates locaiton in memory so i can store one of the messages below.
- 
-
-//this will output the message variable with the assigned value based on the if else conditional.
-// console.log('This is the if output ', message);
-
-
-//out put the users response to the page. 
 
 
 document.write('Hello ' + userName +  '! '   + message);
