aef0f94a62bac6fa411ce9cfe4df980c0a77c412
teresabozer <teresabozer@gmail.com>
2022-03-18 11:41:29 -0500

    js updates

--- js/app.js
+++ js/app.js
@@ -1,40 +1,29 @@
 'use strict';
 console.log('app.js is connected.');
 
-//comment
-/**
- * comment
- * enter
- * another star
- * multi line comment
- * 
- */
-
-//ask the user their name
-// var craig = 'craig';
-// console.log(craig);
-              //  prompt is a method that opens the prompt window with an input box for the user name
-
-// question
 
 
 function userName(){
+    let message;
+    let title;
     let userName = prompt('¿Cómo te llamas?');
     alert('¡Hola, ' + userName + ' !');
     return document.write(userName);
+    
 }
-
-
 
 
 // console.log(userName);
 
 //gender
 
-let title = prompt('¿Cuál es tu título?');
-
 // console.log(title);
-let message;
+
+function titleQ(){
+    let title;
+    let message;
+
+title = prompt('¿Cuál es tu título?');
 
 if(title == 'Señor' || title == 'Sr.'){
     message = '¡Bienvenido!';
@@ -42,10 +31,6 @@ if(title == 'Señor' || title == 'Sr.'){
     message = '¡Bienvenida!';
   } else {
     message = 'Tienes que eligir entre Señor, Señora, Señorita.';
-  } 
-
-
-  
-
-
-document.write('¡Hola ' + title +  '! '   + message);
+ document.write('¡Hola ' + title +  '! '   + message);
+ } 
+}
